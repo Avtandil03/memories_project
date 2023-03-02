1feee942685f11d2e3d778129720c6e0cfac201a
Avtandil <avtandilqazxswedc@gmail.com>
2023-03-02 19:46:40 +0300

    add createPost function

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,11 +1,9 @@
 export default (posts = [], action ) => {
   switch (action.type) {
     case 'FETCH_ALL':
-      console.log(action.payload + 'this action.payload')
       return action.payload
     case 'CREATE':
-      console.log(posts, ' this is posts')
-      return posts
+      return [...posts, action.payload]
     default: 
       return posts
   }
