fa4127a51088e99686b672211ddf0a3f1653a915
Avtandil <avtandilqazxswedc@gmail.com>
2023-03-06 20:36:02 +0300

    improve code & security

--- server/index.js
+++ server/index.js
@@ -3,18 +3,20 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL ='mongodb+srv://avtandil:YDuF8umJ@cluster0.xs2pvt6.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL
 
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log('listenint port -' + PORT) ))
   .catch((error) => console.log(error.message))
