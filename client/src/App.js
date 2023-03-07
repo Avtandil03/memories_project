eb160e288674cc09f3334d98e5bd4018e1fcd4c7
Avtandil <avtandilqazxswedc@gmail.com>
2023-03-07 12:56:57 +0300

    fix route bug

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Container} from '@material-ui/core'
 import { BrowserRouter,  Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar'
 import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
       <Container maxWidth="lg">
         <Navbar />
         <Routes>
-          <Route path="/" exact element={Home}/>
-          <Route path="/auth" exact element={Auth}/>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/auth" exact element={<Auth/>}/>
         </Routes>
       </Container>
     </BrowserRouter>
