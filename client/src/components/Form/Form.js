24b6d8de6b7e866097d9c4e6d154b353884043ef
Avtandil <avtandilqazxswedc@gmail.com>
2023-03-07 20:09:43 +0300

    edit form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -10,11 +10,12 @@ import useStyles from "./syles";
 
 const Form = ({setCurrentID, currentId}) => {
   const [postData, setPostData] = useState({
-    creator: '', title: '', message: '', tags: '', selectedFile: '',
+     title: '', message: '', tags: '', selectedFile: '',
   })
   const post = useSelector((state) => currentId ? state.posts.find(p => p._id === currentId) : null)
   const classes = useStyles();
   const dispatch = useDispatch()
+  const user = JSON.parse(localStorage.getItem('profile'))
 
   useEffect(() => {
     if(post) setPostData(post)
@@ -23,16 +24,26 @@ const Form = ({setCurrentID, currentId}) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     if(currentId){
-      dispatch(updatePost( currentId, postData))
+      dispatch(updatePost( currentId, {...postData, name: user?.result?.name }))
     }else{
-      dispatch(createPost(postData))
+      dispatch(createPost({...postData, name: user?.result?.name }))
     }
     clear()
   };
 
   const clear = () => {
     setCurrentID(null)
-    setPostData({creator: '', title: '', message: '', tags: '', selectedFile: '',})
+    setPostData({ title: '', message: '', tags: '', selectedFile: '',})
+  }
+
+  if(!user?.result?.name) {
+    return(
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Please Sign In to create your own memories  and like other's memories
+        </Typography>
+      </Paper>
+    )
   }
 
   return (
@@ -44,15 +55,10 @@ const Form = ({setCurrentID, currentId}) => {
         onSubmit={(e) => handleSubmit(e)}
       >
         <Typography variant="h6">{currentId ? 'Editing' : 'Creating'}  a Memory </Typography>
-        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator}
-          onChange={(e) => setPostData({...postData, creator: e.target.value})}
-        ></TextField>
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title}
           onChange={(e) => setPostData({...postData, title: e.target.value})}
         ></TextField>
-        <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message}
-          onChange={(e) => setPostData({...postData, message: e.target.value})}
-        ></TextField>
+        <TextField name="message" variant="outlined" label="Message" fullWidth multiline minRows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags}
           onChange={(e) => setPostData({...postData, tags: e.target.value.split(',')})}
         ></TextField>
